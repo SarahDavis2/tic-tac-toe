149350dfb891f43d15aa4238ec99687b7c7c8f2d
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-09-26 18:18:13 -0500

    Feat: js psuedocode

---
+++ script/script.js
@@ -0,0 +1,27 @@
+/* MAIN CODE */
+
+// Gameboard
+    // Create 3x3 array board filled with empty cell objects
+    // showBoard()
+    // availableCells();
+
+// Cell
+    // Initial marker is NULL
+    // addMarker() from either player
+    // getMarker()
+
+// Players
+    // Player1
+    // Player2
+    // getActivePlayer()
+    // switchPlayerTurn()
+
+// GameController
+    // Create Gameboard()
+    // getActivePlayer()
+    // showBoard()
+    // addMarker()
+    // Determine Game End/Winner
+    // Render
+
+/* DOM - UI */
